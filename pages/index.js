888a20d454f0069afaedd550a7defe5a4a8a86c0
Mary <marinakhripankova@gmail.com>
2023-05-06 15:31:48 +0300

    pr4 v2.1

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,26 @@
-const openPopupBtn = document.querySelector('.profile__editbutton_popup-open');
+const openPopupBtn = document.querySelector('.profile__edit-button_popup_opened');
 const popup = document.querySelector('.popup');
 const popupCloseBtn = document.querySelector('.popup__close-btn');
 let formElement = document.querySelector('.popup__form');
-let popupName = document.querySelector('.popup__input_name');
-let popupJob = document.querySelector('.popup__input_job');
+let popupName = document.querySelector('.popup__input_el_name');
+let popupJob = document.querySelector('.popup__input_el_job');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
 let popupEditBtn = document.querySelector('.popup__save-btn');
 
 
-popupName.value = profileName.textContent;
-popupJob.value = profileJob.textContent;
+
 
 function openPopup() {   
+    popupName.value = profileName.textContent;
+    popupJob.value = profileJob.textContent;
     popup.classList.add('popup_opened');
 }
 
 function closePopup(evt) {
     const isOverlay = evt.target.classList.contains('popup');
     const isCloseBtn = evt.target.classList.contains('popup__close-btn');
-    
+
     if (isOverlay || isCloseBtn) {
         popup.classList.remove('popup_opened'); 
     } 
@@ -31,8 +32,8 @@ popupCloseBtn.addEventListener('click', closePopup);
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
-    popupName = document.querySelector('.popup__input_name').value;
-    popupJob = document.querySelector('.popup__input_job').value;
+    popupName = document.querySelector('.popup__input_el_name').value;
+    popupJob = document.querySelector('.popup__input_el_job').value;
 
     profileName.textContent = popupName;
     profileJob.textContent = popupJob;
